46d46e9a3264763024dd059a694848fece571283
moabaka <moatamidbakkali1@gmail.com>
2023-05-16 15:07:17 +0100

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,36 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+import './App.css'
+
+function App() {
+
+
+  return (
+   <>
+      <div className='login'>
+        <form action="POST">
+          <ul>
+            <li>
+              <label htmlFor="user">user</label>
+              <input type="text" id='user' />
+            </li>
+            <li>
+            <label htmlFor="user">password</label>
+              <input type="password" id='pass' />
+            </li>
+            <button onClick={
+              console.log("logged in")
+            }>
+              login 
+            </button>
+          </ul>
+        </form>
+      </div>
+   
+   </>
+      
+  )
+}
+
+export default App
